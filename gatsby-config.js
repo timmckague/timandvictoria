657cc28d3e937271310214cfd7d1d7fe02ef9295
Tim McKague <tim.mckague@gmail.com>
2019-01-19 22:54:51 +0000

    Add Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-132764691-1",
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
