b8503b09435cee88d00911ad28775ea1be00942a
Tim McKague <tim.mckague@gmail.com>
2019-01-19 22:31:08 +0000

    Update Site Meta Data Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'Tim and Victoria',
+    description: 'A collection of our adventures',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
