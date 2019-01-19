c70ad86eca83dddec438b5ee143a1dd77c9a98c9
Tim McKague <tim.mckague@gmail.com>
2019-01-19 22:12:45 +0000

    Center About Us Title and Add Spacing Around Content

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -12,10 +12,10 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
       <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
+            <h1 className="has-text-weight-light is-size-3 has-text-centered">
+              {title}
+            </h1>
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
               <PageContent className="content" content={content} />
             </div>
           </div>
