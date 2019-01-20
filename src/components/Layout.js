45b0877a6bc6ba971f8b8340b476fb95060a0fb1
Tim McKague <tim.mckague@gmail.com>
2019-01-20 12:32:06 +0000

    Update og-image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,7 +34,7 @@ const TemplateWrapper = ({ children }) => (
 	        <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
+          <meta property="og:image" content="/img/blog_aboutus2.jpg" />
         </Helmet>
         <Navbar />
         <div>{children}</div>
