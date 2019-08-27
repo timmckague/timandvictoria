f175e8da0c4d586b14c9e851aeba9637ddd67aab
Tim McKague <tim.mckague@gmail.com>
2019-08-26 23:01:14 -0400

    Upgrade Dependencies

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,10 @@
-import CMS from 'netlify-cms'
+import CMS from "netlify-cms-app";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.init();
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
